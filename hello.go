07507cfbfe36af1243f8498bb2112ce7b196992d
harry <harrytsao12345@gmail.com>
2024-05-11 09:12:43 +0800

    Wrap up

--- hello.go
+++ hello.go
@@ -4,21 +4,42 @@ import (
 	"fmt"
 )
 
-const spanish = "Spanish"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+// cretes prefix string and returns zero or whatever it's set to
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
 	fmt.Println(Hello("jeff", ""))
 }
+
+/*
+Test Driven Development (TDD) ⇒  Write a failing test and see it fail so we know we
+have written a relevant test for our requirements and seen that it produces an easy
+to understand description of the failure ⇒ Write the smallest amount of code to
+pass the case ⇒ Refactor to ensure well-crafted code
+*/
