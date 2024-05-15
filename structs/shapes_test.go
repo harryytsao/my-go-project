16570852b59fa4ff8d313061ca4d2a5c65eea056
harry <harrytsao12345@gmail.com>
2024-05-15 12:08:27 +0800

    structs and interfaces

---
+++ structs/shapes_test.go
@@ -0,0 +1,62 @@
+package main
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	hasArea := 40.0
+
+	if got != hasArea {
+		t.Errorf("got %.2f hasArea %.2f", got, hasArea)
+	}
+}
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g hasArea %g", got, tt.hasArea)
+			}
+		})
+
+	}
+
+}
+
+// func TestArea(t *testing.T) {
+
+// 	checkArea := func(t testing.TB, shape Shape, hasArea float64) {
+// 		t.Helper()
+// 		got := shape.Area()
+// 		if got != hasArea {
+// 			t.Errorf("got %g hasArea %g", got, hasArea)
+// 		}
+// 	}
+
+// 	t.Run("rectangles", func(t *testing.T) {
+// 		rectangle := Rectangle{12, 6}
+// 		checkArea(t, rectangle, 72.0)
+// 	})
+
+// 	t.Run("circles", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		checkArea(t, circle, 314.1592653589793)
+// 	})
+
+// }
